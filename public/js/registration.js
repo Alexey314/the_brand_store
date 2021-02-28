1f5885ae7254549d6373ece1dca507eba9933928
apotapov <apotapovxx@mail.ru>
2021-02-28 19:45:41 +0300

    chore: add register form validation handler

--- public/js/registration.js
+++ public/js/registration.js
@@ -2,6 +2,39 @@
 
 import UserRegistrationData from './user_registration_data.mjs';
 
-const userRegistrationData = new UserRegistrationData;
+const registrationFormEl = document.querySelector(".registration-form");
 
+const firstNameInputEl = document.querySelector(".registration-form__input-text_first-name");
+const lastNameInputEl = document.querySelector(".registration-form__input-text_last-name");
+const phoneNumberInputEl = document.querySelector(".registration-form__input-text_tel");
+const emailInputEl = document.querySelector(".registration-form__input-text_email");
+const passwordInputEl = document.querySelector(".registration-form__input-text_password");
+
+
+registrationFormEl.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const userRegistrationData = new UserRegistrationData;
+
+    if ( !userRegistrationData.setFirstName(firstNameInputEl.value) ){
+
+    }
+
+    if ( !userRegistrationData.setLastName(lastNameInputEl.value) ){
+
+    }
+
+    if ( !userRegistrationData.setPhoneNumber(phoneNumberInputEl.value) ){
+
+    }
+
+    if ( !userRegistrationData.setEmail(emailInputEl.value) ){
+
+    }
+
+    if ( !userRegistrationData.setPassword(passwordInputEl.value) ){
+
+    }
+
+    console.log(userRegistrationData);
+});
 
